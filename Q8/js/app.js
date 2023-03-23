bbea1f8afcfd4b69a7669a80099c93181169c5d8
mint5150 <musia.home@gmail.com>
2023-03-23 20:42:18 +0900

    master

--- Q8/js/app.js
+++ Q8/js/app.js
@@ -6,11 +6,9 @@ $(function () {
   $(".search-btn").on("click", function () {
     let currentSearchInput = $("#search-input").val();
   if(currentSearchInput !== lastSearchInput) {
-    (
       page = 1,
       $(".lists").empty(),
       lastSearchInput = currentSearchInput
-    );
   } else {
     page++;
   }
@@ -28,37 +26,32 @@ $(function () {
   //通信成功処理
   function showResult(result) {
     $(".message").remove();
-    if(null === (result[0].items)) {
-      void 0;
-    } else {
-      result[0].items.length;
-    }
-    if(0 < result[0].items.length) {
+    if(result[0].items.length > 0) {
       $.each(result[0].items, function (index, item) {
-        let html = '<li class="lists-item"><div class="list-inner"><p>\u30bf\u30a4\u30c8\u30eb\uff1a';
+        let html = '<li class="lists-item"><div class="list-inner"><p>';
         if (item.title) {
           html += item.title;
         } else {
-          html += "\u30bf\u30a4\u30c8\u30eb\u4e0d\u660e";
+          html += "";
         }
-        html += "</p><p>\u4f5c\u8005\uff1a";
+        html += "</p><p>";
         if (item['dc:creator']) {
           html += item["dc:creator"];
         } else {
-          html += "\u4f5c\u8005\u4e0d\u660e";
+          html += "";
         }
-        html += "</p><p>\u51fa\u7248\u793e\uff1a";
+        html += "</p><p>";
         if (item["dc:publisher"]) {
           html += item["dc:publisher"][0];
         } else {
-          html += "\u51fa\u7248\u793e\u4e0d\u660e";
+          html += "";
         }
         html += '</p><a href="';
-        + (item.link["@id"] +'" target="_blank">\u66f8\u7c4d\u60c5\u5831</a></div></li>');
+        + (item.link["@id"] +'" target="_blank"></a></div></li>');
         $(".lists").prepend(html)
       })
     } else {
-      $(".lists").before('<div class="message">\u691c\u7d22\u7d50\u679c\u304c\u898b\u3064\u304b\u308a\u307e\u305b\u3093\u3067\u3057\u305f\u3002<br>\u5225\u306e\u30ad\u30fc\u30ef\u30fc\u30c9\u3067\u691c\u7d22\u3057\u3066\u4e0b\u3055\u3044\u3002</div>');
+      $(".lists").before('<div class="message"></div>');
     }
   };
 
