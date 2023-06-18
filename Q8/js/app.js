6c0e4115445a65a3a4781d0f0581f273d015b836
mint5150 <musia.home@gmail.com>
2023-06-19 02:09:21 +0900

    master

--- Q8/js/app.js
+++ Q8/js/app.js
@@ -9,8 +9,8 @@ $(function () {
   if(currentSearchInput === lastSearchInput) {
       page = 0
       $(".lists").empty();
-  } else {
-    page++;
+  /*} else {
+    page++;*/
   }
 
   //ajax通信
@@ -18,8 +18,8 @@ $(function () {
     .done(function (data) {
       showResult(data["@graph"])
     })
-    .fail(function (jqXhr) {
-      showError(jqXhr)
+    .fail(function (jqError) {
+      showError(jqError)
     })
   });
 
@@ -27,16 +27,18 @@ $(function () {
   function showResult(result) {
     $(".lists").empty();
     $(".message").remove();
+
     if(result[0]["opensearch:totalResults"] > 0) {
       $.each(result[0].items, function (index, item) {
         const title = item.title ? item.title : "不明";
         const creator = item['dc:creator'] ? item["dc:creator"] : "不明";
         const publisher = item["dc:publisher"] ? item["dc:publisher"][0] : "不明";
-        let url = '</p><a href="' + (item.link["@id"] + '" target="_blank">書籍情報</a></div></li>');
+        const url = '</p><a href="' + (item.link["@id"] + '" target="_blank">書籍情報</a></div></li>');
         const htmlResult = '<li class="lists-item"><div class="list-inner"><p>タイトル：' + title + "</p><p>" + "著者：" + creator + "</p><p>" + "出版社：" + publisher + url;"</p>";
 
         $(".lists").prepend(htmlResult)
       })
+
     } else {
       page = 0
       $(".message").remove();
@@ -45,13 +47,13 @@ $(function () {
   };
 
   //通信失敗処理
-  function showError(jqXhr) {
+  function showError(ajaxError) {
   $(".lists").empty();
   $(".message").remove();
-  if (0 === jqXhr.status) {
+  if (0 === ajaxError.status) {
       $(".lists").before('<div class="message">正常に通信できませんでした。<br>インターネットの接続の確認をしてください。</div>');
     } else {
-      if (400 === jqXhr.status) {
+      if (400 === ajaxError.status) {
         $(".lists").before('<div class="message">検索キーワードが有効ではありません。<br>1文字以上で検索して下さい。</div>');
       } else {
         $(".lists").before('<div class="message">予期せぬエラーが起きました。<br>再読み込みを行ってください。</div>');
